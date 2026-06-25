00329cb74b9fdb8c5ecebf499c578233fd620770
Fardan Nozami <fardan.nozami@gmail.com>
2026-06-25 20:53:05 +0700

    feat: define API routes for products and transactions controllers

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('products', ProductController::class);
+Route::apiResource('transactions', TransactionController::class)->only(['index', 'show', 'store']);
